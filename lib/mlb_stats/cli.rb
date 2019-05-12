171a38a645ac2331bb89747dd4dc4ab5f6110157
Gregory Losco <gtlosco@gmail.com>
2019-05-12 21:16:18 +0000

    list_teams refactored

--- lib/mlb_stats/cli.rb
+++ lib/mlb_stats/cli.rb
@@ -8,13 +8,14 @@ class MlbStats::CLI
   
   def list_teams
     
-    puts "MLB Teams"
-    puts <<-DOC.gsub /^\s*/, ''
-    1. Boston Red Sox - AL East - 1st Place
-    2. New York Yankees - AL East - 2nd Place
-    DOC
+    # puts "MLB Teams"
+    # puts <<-DOC.gsub /^\s*/, ''
+    # 1. Boston Red Sox - AL East - 1st Place
+    # 2. New York Yankees - AL East - 2nd Place
+    # DOC
     
     @teams = MlbStats::Team.today
+    @teams.each.with_index(1) {|team, i| puts "#{i}. #{team.name} - #{team.division} - #{team.division_ranking} - #{team.record}"}
   end
   
   def menu
@@ -22,15 +23,13 @@ class MlbStats::CLI
     while input != "exit"
     puts "Enter the number of the team you'd like more info on or type list to see teams again or type exit to exit:"
     input = gets.strip.downcase
-      case input
-      when "1"
-        puts "More info on team 1..."
-      when "2"
-        puts "More info on team 2..."
-      when "list"
-        list_teams
-      else 
-        puts "Not sure what you want, type list or exit"
+    
+   if input.to_i > 0 
+     puts @teams[input.to_i-1]
+   elsif input == "list"
+    list_teams
+  else       
+    puts "Not sure what you want, type list or exit"
       end
     end
   end
