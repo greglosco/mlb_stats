b409cb114a9302cf8aec077aae620aa802bc235b
Gregory Losco <gtlosco@gmail.com>
2019-05-12 19:40:55 +0000

    permission granted, CLI infrastructure started

--- lib/mlb_stats/cli.rb
+++ lib/mlb_stats/cli.rb
@@ -1,7 +1,21 @@
 class MlbStats::CLI 
   
   def call 
+    list_teams
+    menu
+  end
+  
+  def list_teams
+    
     puts "MLB Teams"
+    puts <<-DOC.gsub /^\s*/, ''
+    1. Boston Red Sox - AL East - 1st Place
+    2. New York Yankees - AL East - 2nd Place
+    DOC
+  end
+  
+  def menu
+    puts "Enter the number of the team you'd like more info on:"
   end
   
 end
