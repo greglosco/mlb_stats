0b27444b70492e93f3d0d22038ac097d95ff9e75
Gregory Losco <gtlosco@gmail.com>
2019-05-14 04:43:35 +0000

    commented out conference and division pages

--- lib/mlb_stats/conference.rb
+++ lib/mlb_stats/conference.rb
@@ -1,24 +1,24 @@
-class MlbStats::Conference
+# class MlbStats::Conference
   
-  attr_accessor :name
+#   attr_accessor :name
   
-  @@all = []
+#   @@all = []
   
-  def self.new_from_index(i)
-    self.new(i.css("h3").text,)
-  end
+#   def self.new_from_index(i)
+#     self.new(i.css("h3").text,)
+#   end
   
-  def initialize(name=nil)
-    @name = name
-    @@all << self 
-  end
+#   def initialize(name=nil)
+#     @name = name
+#     @@all << self 
+#   end
   
-  def self.all
-    @@all
-  end
+#   def self.all
+#     @@all
+#   end
   
-  def self.find(id)
-    self.all[id-1]
-  end
+#   def self.find(id)
+#     self.all[id-1]
+#   end
   
-end
+# end
--- lib/mlb_stats/division.rb
+++ lib/mlb_stats/division.rb
@@ -1,26 +1,26 @@
-class MlbStats::Division
+# class MlbStats::Division
   
-  attr_accessor :name
+#   attr_accessor :name
   
-  @@all = []
+#   @@all = []
   
-  def self.new_from_index(i)
-    self.new(
-      i.css("h4").text,
-      )
-  end
+#   def self.new_from_index(i)
+#     self.new(
+#       i.css("h4").text,
+#       )
+#   end
   
-  def initialize(name=nil)
-    @name = name
-    @@all << self 
-  end
+#   def initialize(name=nil)
+#     @name = name
+#     @@all << self 
+#   end
   
-  def self.all
-    @@all
-  end
+#   def self.all
+#     @@all
+#   end
   
-  def self.find(id)
-    self.all[id-1]
-  end
+#   def self.find(id)
+#     self.all[id-1]
+#   end
   
-end
+# end
